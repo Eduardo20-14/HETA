7fc50cb3ac977ffa119805ca186e0d9f9e7c946a
Eduardo20-14 <60802123+Eduardo20-14@users.noreply.github.com>
2020-02-07 22:47:34 -0600

    Practica 1
    Programa el cual mediante una ventana principal contiene un botón el cual realiza una acción (evento) que al dar clic sobre el se abre un cuadro de dialogo con información mía como estudiante.

---
+++ Ventana.java
@@ -0,0 +1,56 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+ 
+public class Ventana extends JFrame implements ActionListener
+{
+    private JLabel texto;                  
+    private JButton boton;
+    
+    public Ventana()
+    {
+    	super();                    
+        configurarVentana();        
+        inicializarComponentes();
+    }
+    
+    private void configurarVentana()
+   	{
+   		this.setTitle("Practica1");                   
+        this.setSize(310, 210);                                
+        this.setLocationRelativeTo(null);                       
+        this.setLayout(null);                                   
+        this.setResizable(false);                               
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    private void inicializarComponentes()
+    {
+        texto = new JLabel();
+        boton = new JButton(); 
+        texto.setText("Eduardo M. B.\n Ing. ISC \n 181160154");    
+        texto.setBounds(40, 40, 200, 25);  
+        boton.setText("Mensaje");   
+        boton.setBounds(50, 100, 200, 30);  
+        boton.addActionListener(this);       
+        this.add(texto);
+        this.add(boton);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        JOptionPane.showMessageDialog(this, "No. Control\n 181160154 \nDireccion: Barrio del Sol \nTelefono: 9581420728. \nGenero: Masculino");
+    }
+    
+    public static void main(String[] args) {
+    	
+    	Ventana V=new Ventana();
+    	V.setVisible(true);
+    }
+}
